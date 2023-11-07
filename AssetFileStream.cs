444fe1776058ba951d5e3322602bde0a1b21a85b
Liu.Yandong.Hanks <liu.yandong.hanks@outlook.com>
2023-11-07 20:55:49 +0800

    进存储像素数据

--- AssetFileStream.cs
+++ AssetFileStream.cs
@@ -1,7 +1,9 @@
 ﻿using Resource.Package.Assets.Common;
 using Resource.Package.Assets.Secure;
 using Resource.Package.Assets.Version;
+using StbImageSharp;
 using System.Drawing;
+using System.IO;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +26,7 @@ namespace Resource.Package.Assets
             var meta = new FileHeader();
             meta.CompressOption = compressionOption;
             meta.Magic = MAGIC;
-            meta.Version = new byte[] { 1, 0, 1 };
+            meta.Version = new byte[] { 1, 0, 2 };
             meta.TableDataSize = 0;
             meta.TableDataAddr = 24;
             meta.NumberOfFiles = 0;
@@ -115,7 +117,7 @@ namespace Resource.Package.Assets
                     header.TableDataAddr = reader.ReadUInt32();
                     header.TableDataSize = reader.ReadUInt32();
                     this.ReadIndex(reader);
-                    header.Version = new Byte[] { 1, 0, 1 };
+                    header.Version = new Byte[] { 1, 0, 2 };
                 }
             }
             catch (Exception ex)
@@ -139,6 +141,26 @@ namespace Resource.Package.Assets
             return node;
         }
 
+        public Boolean Recycle()
+        {
+            var IsChanged = false;
+            while (this.NumberOfFiles > 0)
+            {
+                var Index = this.NumberOfFiles - 1;
+                var info = this.Infomations[(Int32)Index];
+                if (info.lpData > 0) break;
+                this.Infomations.Remove(info);
+                this.header.NumberOfFiles--;
+                IsChanged = true;
+            }
+            if (IsChanged)
+            {
+                this.Save();
+            }
+            return IsChanged;
+        }
+
+
 
         public IReadOnlyDataBlock Read(UInt32 index)
         {
@@ -152,7 +174,9 @@ namespace Resource.Package.Assets
                 node.Unknown2 = info.Unknown2;
                 node.OffsetX = info.OffsetX;
                 node.OffsetY = info.OffsetY;
-                if (info.lpSize > 0)
+                node.Width = info.Width;
+                node.Height = info.Height;
+                if (info.lpData > 0 && info.lpSize > 0)
                 {
                     var data = new Byte[info.lpSize];
                     reader.BaseStream.Position = info.lpData;
@@ -220,6 +244,7 @@ namespace Resource.Package.Assets
 
         private ImageTypes ParseImageFormat(Byte[] data)
         {
+            if (data.Length == 0) return ImageTypes.Unknown;
             if (data[0] == 0x42 && data[1] == 0x4D) return ImageTypes.BMP;
             if (data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4E && data[3] == 0x47) return ImageTypes.PNG;
             if (data[0] == 0xFF && data[1] == 0xD8 && data[2] == 0xFF) return ImageTypes.JPG;
@@ -231,14 +256,23 @@ namespace Resource.Package.Assets
 
 
 
-
         private async Task<Byte[]> Compressing(DataBlock item, Action report = null)
         {
             Byte[] bytes = item.Data;
-            if (header.CompressOption == CompressionOption.MuchPossibleCompress || header.CompressOption == CompressionOption.MustCompressed)
+            if (bytes.Length > 0)
             {
-                bytes = await ZLib.Compress(item.Data);
-                if (bytes.Length > item.Data.Length && header.CompressOption == CompressionOption.MuchPossibleCompress)
+                ImageResult imageResult = ImageResult.FromMemory(bytes, ColorComponents.RedGreenBlueAlpha);
+                // Png 图片需要处理Alpha预乘
+                if(item.lpType == ImageTypes.PNG) AlphaUtil.PremultiplyAlpha(imageResult.Data);
+                item.Width = imageResult.Width;
+                item.Height = imageResult.Height;
+                item.Data = imageResult.Data;
+                bytes = imageResult.Data;
+            }
+            if (bytes.Length > 0 && header.CompressOption == CompressionOption.MuchPossibleCompress || header.CompressOption == CompressionOption.MustCompressed)
+            {
+                bytes = await ZLib.Compress(bytes);
+                if (bytes.Length >= item.Data.Length && header.CompressOption == CompressionOption.MuchPossibleCompress)
                 {
                     bytes = item.Data;
                 }
@@ -260,9 +294,11 @@ namespace Resource.Package.Assets
             };
             foreach (var item in items)
             {
+                item.lpType = ParseImageFormat(item.Data);
                 tasks.Add(Compressing(item, report));
             }
             Task.WaitAll(tasks.ToArray());
+            GC.Collect();
             using (var writer = new BinaryWriter(fileStream, Encoding.UTF8, true))
             {
                 for (var i = 0; i < tasks.Count; i++)
@@ -270,16 +306,25 @@ namespace Resource.Package.Assets
                     var item = items[i];
                     var bufData = tasks[i].Result;
                     var info = this.Apply(item.Index);
-                    info.lpType = ParseImageFormat(item.Data);
+                    info.Width = item.Width;
+                    info.Height = item.Height;
+                    info.lpType = item.lpType;
                     info.lpRawSize = (UInt32)item.Data.Length;
                     info.OffsetX = item.OffsetX;
                     info.OffsetY = item.OffsetY;
                     info.lpRenderType = item.lpRenderType;
                     info.lpSize = (UInt32)bufData.Length;
-                    info.lpData = header.TableDataAddr;
-                    header.TableDataAddr = info.lpData + info.lpSize;
-                    writer.Seek((Int32)info.lpData, SeekOrigin.Begin);
-                    writer.Write(bufData);
+                    if (info.lpSize > 0)
+                    {
+                        info.lpData = header.TableDataAddr;
+                        header.TableDataAddr = info.lpData + info.lpSize;
+                        writer.Seek((Int32)info.lpData, SeekOrigin.Begin);
+                        writer.Write(bufData);
+                    }
+                    else
+                    {
+                        info.lpData = 0;
+                    }
                     report();
                 }
                 this.WriteIndex(writer);
